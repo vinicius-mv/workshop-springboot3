dd35d089e33a436462f2ca75946759d36bd3dbda
Vinícius Vieira <viniciusmv91@gmail.com>
2025-02-15 20:34:31 -0300

    add UserService for user management and update UserResource to use service methods

--- src/main/java/dev/thisisvinicius/productsapi/resources/UserResource.java
+++ src/main/java/dev/thisisvinicius/productsapi/resources/UserResource.java
@@ -1,22 +1,32 @@
 package dev.thisisvinicius.productsapi.resources;
 
 import dev.thisisvinicius.productsapi.entities.User;
+import dev.thisisvinicius.productsapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Array;
-import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User[]> findAll() {
-        User u1 = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
-        var users = new User[] { u1 };
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = this.service.findAll();
         return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = this.service.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
