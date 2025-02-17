80571313e48b030a2b6a6ff82bf17eda5e0deb8b
Vinícius Vieira <viniciusmv91@gmail.com>
2025-02-16 23:07:41 -0300

    add @JsonIgnore annotation to password field in User model

--- src/main/java/dev/thisisvinicius/productsapi/entities/User.java
+++ src/main/java/dev/thisisvinicius/productsapi/entities/User.java
@@ -19,6 +19,7 @@ public class User implements Serializable {
     private String name;
     private String email;
     private String phone;
+    @JsonIgnore
     private String password;
 
     @JsonIgnore
